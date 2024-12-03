41869e0fdbc15151c9bdeb3bb7bbc8b1971ca875
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2024-12-02 19:15:12 -0800

    EventMap: Remove unused events

--- features/QOL/MiscShit.js
+++ features/QOL/MiscShit.js
@@ -1,11 +1,14 @@
 import Feature from "../../core/Feature";
 import Settings from "../../data/Settings";
 
-/** @type {HashMap<Number: EntityTypeByte, (() => Boolean)|Boolean: always/never>} */
+/** 
+ * @see {https://github.com/Marcelektro/MCP-919/blob/1717f75902c6184a1ed1bfcd7880404aab4da503/src/minecraft/net/minecraft/entity/EntityTrackerEntry.java} ctrl-f S0EPacketSpawnObject
+ * @type {HashMap<Number: EntityTypeByte, (() => Boolean)|Boolean: always/never>} 
+ */
 const blacklist = new HashMap()
 Object.entries({
     1: true, // Boat,
-    2: false, // Item (Dropped items & dungeon secrets)
+    // 2: false, // Item (Dropped items & dungeon secrets)
     10: true, // MineCart
     50: () => Settings().removeTnt, // TntPrimed (SuperBoomTnt & T5 Phase 4)
     51: () => Settings().removeCrystals, // EnderCrystal (Ender Dragons & F/M7 Phase 1)
@@ -16,28 +19,23 @@ Object.entries({
     64: () => Settings().removeFireballs, // SmallFireball
     66: () => Settings().removeWitherSkulls, // WitherSkull
     70: () => Settings().removeFallingBlocks, // FallingBlock
-    71: false, // ItemFrame ("Why can't I do Tic-Tac-Toe")
+    // 71: false, // ItemFrame ("Why can't I do Tic-Tac-Toe")
     72: true, // EnderEye
     73: true, // Potion
     75: true, // ExpBottle
+    76: () => Settings().removeRockets, // Rocket
     77: true, // Leash
-    78: false, // ArmorStand (LET ME CLICK EXPERIMENTATION TABLE)
-    86: () => Settings().removeRockets, // Rocket
-    90: false, // FishHook ("Is my rod out?")
-    // Painting (Lovely separate packet)
-    // ExpOrbs (Lovely separate packet)
+    // 78: false, // ArmorStand (LET ME CLICK EXPERIMENTATION TABLE)
+    // 90: false, // FishHook ("Is my rod out?")
 }).forEach(([k, v]) => blacklist.put(k, v))
 
 new Feature({setting: "miscShit"})
     .addEvent("spawnObject", (entityType, event) => {
         const value = blacklist.get(entityType)
         if (!value) return
-
-        if (
-            (typeof(value) === "function" && value())
-            ||
-            (typeof(value) === "boolean" && value)
-        ) cancel(event)
+        if (typeof(value) === "function" && !value()) return
+        
+        cancel(event)
     })
 
     .addEvent("spawnPainting", cancel)
--- libs/CustomEventFactory/EventMap.js
+++ libs/CustomEventFactory/EventMap.js
@@ -31,16 +31,6 @@ createEvent("serverTick", (fn) =>
     }).setFilteredClass(net.minecraft.network.play.server.S32PacketConfirmTransaction)
 )
 
-createEvent("entityLoad", (fn, clazz) => 
-    register(net.minecraftforge.event.entity.EntityJoinWorldEvent, (event) => {
-        const entity = event.entity
-        if (clazz && !(entity instanceof clazz)) return
-    
-        const entityID = entity.func_145782_y()
-        fn(entity, entityID)
-    })
-)
-
 createEvent("spawnDamageTag", (fn) =>
     register("packetReceived", (packet, event) => {
         if (packet.func_149025_e() !== 30) return
@@ -50,7 +40,6 @@ createEvent("spawnDamageTag", (fn) =>
     }).setFilteredClass(net.minecraft.network.play.server.S0FPacketSpawnMob)
 )
 
-/** @see {https://github.com/Marcelektro/MCP-919/blob/1717f75902c6184a1ed1bfcd7880404aab4da503/src/minecraft/net/minecraft/entity/EntityTrackerEntry.java} ctrl-f S0EPacketSpawnObject*/
 createEvent("spawnObject", (fn) =>
     register("packetReceived", (packet, event) => 
         fn(packet.func_148993_l(), event)
@@ -141,28 +130,6 @@ createEvent("sideBarChange", (fn, criteria) =>
     }).setFilteredClass(net.minecraft.network.play.server.S3EPacketTeams)
 )
 
-createEvent("tabUpdate", (fn, criteria) => 
-    register("packetReceived", (packet, event) => {
-        const players = packet.func_179767_a() // .getPlayers()
-        const action = packet.func_179768_b() // .getAction()
-
-        if (action !== S38PacketPlayerListItem.Action.UPDATE_DISPLAY_NAME) return
-
-        players.forEach(addPlayerData => {
-            const name = addPlayerData.func_179961_d() // .getDisplayName()
-            
-            if (!name) return
-
-            const formatted = name.func_150254_d() // .getFormattedText()
-            const unformatted = formatted.removeFormatting()
-        
-            if (action !== S38PacketPlayerListItem.Action.UPDATE_DISPLAY_NAME) return
-
-            TextUtil.matchesCriteria(fn, criteria, unformatted, event, formatted)
-        })
-    }).setFilteredClass(S38PacketPlayerListItem)
-)
-
 createEvent("tabAdd", (fn, criteria) => 
     register("packetReceived", (packet, event) => {
         const players = packet.func_179767_a() // .getPlayers()
@@ -190,44 +157,6 @@ createEvent("worldSound", (fn, criteria) =>
 
         TextUtil.matchesCriteria(fn, criteria, name, event)
     }).setFilteredClass(net.minecraft.network.play.server.S29PacketSoundEffect)
-)
-
-createEvent("heldItemChange", (fn) => 
-    register("packetSent", (packet) => {
-        const item = Player.getHeldItem()
-        if (!item) return fn()
-
-        const index = packet.func_149614_c()
-        const attr = ItemUtil.getExtraAttribute(item)
-        const sbID = ItemUtil.getSkyblockItemID(item)
-        
-        fn(item, index, attr, sbID)
-    }).setFilteredClass(net.minecraft.network.play.client.C09PacketHeldItemChange)
-)
-
-createEvent("inventoryUpdate", (fn) => 
-    register("packetReceived", (packet) => {
-        if (packet.func_149175_c() === 0) fn(packet.func_149174_e(), packet.func_149173_d())
-    }).setFilteredClass(net.minecraft.network.play.server.S2FPacketSetSlot)
-)
-
-createEvent("openContainer", (fn) => 
-    register("packetReceived", (packet) => {
-        const windowTitle = packet.func_179840_c().func_150254_d().removeFormatting()
-        const windowID = packet.func_148901_c()
-        const hasSlots = packet.func_148900_g()
-        const slotCount = packet.func_148898_f()
-        const guiID = packet.func_148902_e()
-        const entityID = packet.func_148897_h()
-    
-        fn(windowTitle, windowID, hasSlots, slotCount, guiID, entityID)
-    }).setFilteredClass(net.minecraft.network.play.server.S2DPacketOpenWindow)
-)
-
-createEvent("closeContainer", (fn) => 
-    register("guiClosed", (screen) => {
-        if (screen instanceof net.minecraft.client.gui.inventory.GuiContainer) fn(screen)
-    })
 )
 
 createEvent("containerClick", (fn) => 
